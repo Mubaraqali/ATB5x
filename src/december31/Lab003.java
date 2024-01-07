0e1244c1440e6c77ff14f7f43122358884753501
Mubaraq Ali <mubaraq.ali@gmail.com>
2024-01-07 21:04:53 +0530

    Commit on 07th Jan

--- src/december31/Lab003.java
+++ src/december31/Lab003.java
@@ -1,7 +1,7 @@
 package src.december31;
 
-public class Lab002 {
+public class Lab003 {
     public static void main(String[] args) {
-        System.out.println("git");
+        System.out.println("for Git");
     }
 }
